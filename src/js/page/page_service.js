754c18d91e15ce002985c207fc503ae7a35441f5
李晶 <lijing@bingosoft.net>
2018-05-22 10:28:04 +0800

    支持远程获取页面配置，同时也支持本地获取测试页面配置

--- src/js/page/page_service.js
+++ src/js/page/page_service.js
@@ -1,23 +1,21 @@
 import ax from '../ajax.js';
 import config from '../config.js';
 import buiweex from 'bui-weex';
+const linkapi = require('linkapi');
 
 const pageService={
     get(pageId){
         return new Promise(function(resolve,reject) {
-            /*ax.get(`http://10.200.84.125:3333/project/bui-weex-metaapp/dist/test_page/${pageId}.json`).then(({data})=>{
-                 resolve(data);
-             },()=>{
-                 reject();
-             });*/
             config.readRuntimeConfig().then(runtimeConfig => {
                 ax.get(runtimeConfig["service.metad.api.endpoint"]+`/mp_page/${pageId}`).then(({data})=>{
-                    if(typeof data.layout=="string"){
-                        data.layout = eval('('+data.layout+')');
-                    }
                     resolve(data.layout);
                 },()=>{
-                    reject();
+                    var testPath=`${buiweex.getContextPath()}/test_page/${pageId}.json`;
+                    linkapi.readTextFromFile(testPath, 'utf-8', data => {
+                        resolve(data);
+                    },()=>{
+                        reject();
+                    });
                 });
             });
         })
