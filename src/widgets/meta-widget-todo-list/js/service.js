00b8c3eb8c458f062bbbf20c235b893e9d069eb9
李晶 <lijing@bingosoft.net>
2018-06-20 11:12:38 +0800

    去掉无用的js

--- src/widgets/meta-widget-todo-list/js/service.js
+++
@@ -1,8 +0,0 @@
-import config from '../../../js/config';
-var $resource;
-config.readRuntimeConfig().then(runtimeConfig => {
-    $resource=Vue.resource('v1/tasks/todo',null,null,{root:runtimeConfig["service.activiti.runtime.endpoint"]});
-});
-export default {
-    $resource: $resource
-};
