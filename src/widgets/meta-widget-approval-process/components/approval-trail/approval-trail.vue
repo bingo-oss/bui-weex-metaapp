f443a3c34fb0d6d4a1dc44e38fef41e307d47783
贺嘉文 <hejw@bingosoft.net>
2018-08-24 16:16:17 +0800

    流程轨迹组件样式调整

--- src/widgets/meta-widget-approval-process/components/approval-trail/approval-trail.vue
+++ src/widgets/meta-widget-approval-process/components/approval-trail/approval-trail.vue
@@ -1,11 +1,33 @@
 <template>
 <div class="approval-trail">
+    <div v-for="(item,index) in trail">
+        <div style="flex-direction: row; padding-left: 10px;">
+            <div class="approval-trail-w" :style="item.borderLeftWidth">
+                <div class="approval-trail-title">
+                    <!--<text class="font28">{{item.name}}</text>-->
+                    <div style="flex-direction: row;">
+                        <text class="font28" :style="{width:'300px',color:item.textColor}" v-if="item.name">{{item.name}}</text>
+                        <text class="font28 color-sub" v-if="item.endTime" :style="{color:item.color}" style="text-align: right"> {{item.endTime}}</text>
+                    </div>
+                </div>
+                <div class="approval-trail-details">
+                    <div class="approval-trail-info">
+                        <template>
+                            <text class="font28 color-sub" :style="{color:item.color}" v-if="item.assigneeName">{{item.assigneeName}}</text>
+                            <text class="font28 color-sub" :style="{color:item.color}" v-if="item.opinion">审批意见: {{item.opinion}}</text>
+                        </template>
+                    </div>
+                </div>
+            </div>
+            <div class="_dot" :style="item.dotStyle"></div>
+        </div>
+    </div>
     <scroller class="scroller">
-        <bui-timeline :customStyles="customStyles">
+        <!--<bui-timeline :customStyles="customStyles">
             <bui-timeline-item v-for="(item,index) in trail" :first="index===0" :last="index===trail.length-1 && index!==0" :key="item.id" :color="item.color">
                 <div style="flex-direction:column;">
                     <div class="approval-trail-title">
-                        <!--<text class="font28">{{item.name}}</text>-->
+                        &lt;!&ndash;<text class="font28">{{item.name}}</text>&ndash;&gt;
                         <div style="flex-direction: row;">
                             <text class="font28" :style="{width:'400px',color:item.textColor}" v-if="item.assigneeName">{{item.name}}</text>
                             <text class="font28 color-sub" v-if="item.createTime" style="text-align: right"> {{item.endTime}}</text>
@@ -21,7 +43,7 @@
                     </div>
                 </div>
             </bui-timeline-item>
-        </bui-timeline>
+        </bui-timeline>-->
     </scroller>
 </div>
 </template>
@@ -71,15 +93,28 @@ export default {
                         return {
                             name:item.name,
                             assigneeName:item.assigneeName,
-                            createTime:item.createTime?utils.formatDate(item.createTime.replace(/T/g,' ').replace(/\.[\d]{3}Z/,'')):"",
-                            endTime:item.endTime?utils.formatDate(item.endTime.replace(/T/g,' ').replace(/\.[\d]{3}Z/,'')):"",
+                            createTime:item.createTime?utils.formatDate(item.createTime):"",
+                            endTime:item.endTime?utils.formatDate(item.endTime):"",
                             opinion:item.variables?item.variables.opinion:"",
-                            color:"#DFDFDF",
-                            textColor:"#000"
+                            color:"#ccc",
+                            textColor:"#ccc",
+                            approvalTrailw:{
+
+                            },
+                            dotStyle:{
+                                "background-color":"#DFDFDF"
+                            }
                         }
                     });
-                    _this.trail[(_this.trail.length-1)].color = "#5099F4";//高亮dot
-                    _this.trail[(_this.trail.length-1)].textColor = "#5099F4";//高亮文字
+                    _this.trail.reverse();//数组执行下倒叙
+                    Object.assign(_this.trail[0],{
+                        select:true,
+                        color:"#5099F4",//高亮dot
+                        textColor:"#000",//高亮文字
+                        assigneeName:"审批中",
+                        dotStyle:{"background-color":"#5099F4"}
+                    })
+                    _this.trail[(_this.trail.length-1)].borderLeftWidth = {"border-left-width":0};//不需要显示线条
 
                 })
             }
@@ -100,11 +135,21 @@ export default {
     .color-sub{
         color:#666;
     }
-    .approval-trail-title{ padding-top: 4px;}
+    .approval-trail-title{}
     .approval-trail{
+/*
         flex:1;
+*/
         padding-top:20px;
         padding-left:20px;
+    }
+    .approval-trail-w{
+        flex-direction:column;
+        border-left-style:solid;
+        border-left-width: 1px;
+        border-left-color: #DFDFDF;
+        padding-left: 60px;
+        padding-bottom: 40px;
     }
     .approval-trail-details{
         margin-top:10px;
@@ -112,6 +157,9 @@ export default {
     }
     .approval-trail-info{
         width: 700px;
+    }
+    ._dot{
+        position: absolute; width:22px; height:22px; background-color: #5099F4; border-radius: 22px; left: 0;
     }
     
 </style>
