b89fa923268f13edeae431122e546817984499ec
贺嘉文 <hejw@bingosoft.net>
2018-10-23 20:37:52 +0800

    跳转操作修复

---
+++ src/components/meta_operation/to_operation_operation.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="full-column">
+        <div @click="gotoPage" class="full-column">
+            <slot>
+                <meta-opt-btn :btn-type="btnType" :operation="operation"></meta-opt-btn>
+            </slot>
+        </div>
+    </div>
+</template>
+<script>
+    import _ from '../../js/tool/lodash.js';
+    import Utils from '../../js/tool/utils';
+    import OperationUtils from './js/operation_utils';
+
+    export default {
+        props:{
+            widgetContext:{//由使用操作的部件传入的部件上下文
+                type:Object,
+                required:true
+            },
+            operation:{//操作的定义，必传参数
+                type:Object,
+                required:true
+            },
+            btnType:{//操作按钮的类型
+                type:String
+            }
+        },
+        data(){
+            if(!this.operation.operationId){
+                this.$toast("page参数缺失");
+            }
+            return {
+            };
+        },
+        methods:{
+            gotoPage(){
+                var _widgetCtx = Object.assign(this.widgetContext, this.operation);
+                OperationUtils.execution(this.operation,_widgetCtx,"beforeExecCode").then((res)=>{
+                    if(!this.operation.pageId){
+                        return;
+                    }
+                    var pageId=this.operation.operationId;
+                    var queryParam=_.extend({pageId:pageId,byOperation:true},this.getIdFromContext(),this.operation.queryParams);
+                    this.$push(Utils.pageEntry(),queryParam);
+                    this.$emit("triggered","toPage");
+                    OperationUtils.execution(this.operation,_widgetCtx,"afterExecCode")//执行后
+                });
+            }
+        }
+    }
+</script>
+<style src="../../styles/common.css"></style>
