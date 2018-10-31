e7dcfa3dd1752cae30788f5035fa0f4123f26590
贺嘉文 <hejw@bingosoft.net>
2018-10-31 11:20:48 +0800

    操作按钮图标--若是不具备则默认设置为+

--- src/components/meta_operation/btn/icon_btn.vue
+++ src/components/meta_operation/btn/icon_btn.vue
@@ -1,11 +1,23 @@
 <template>
-    <icon color="white" :name="operation.icon"></icon>
+    <icon v-if="operation.icon" color="white" :name="ion"></icon>
 </template>
 <script>
 export default {
     props:{
         operation:{
             type:Object
+        }
+    },
+    data(){
+      return {
+          ion:"ion-plus"
+      }
+    },
+    watch:{
+        "operation"(val){
+            if(val.icon){
+                this.ion = val.icon;
+            }
         }
     }
 }
