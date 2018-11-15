96473c37da2bd45dcc9c326abc766b361385a810
贺嘉文 <hejw@bingosoft.net>
2018-11-15 18:42:59 +0800

    扩展按钮类型样式

---
+++ src/components/meta_operation/btn/img_btn.vue
@@ -0,0 +1,51 @@
+<template>
+<!--
+    <text>123123123</text>
+-->
+    <div>
+        <image style="width: 40px; height: 40px;" :src="getImageUrl(operation.icon)"></image>
+    </div>
+</template>
+<script>
+    import Util from '../../../js/utils';
+    import Config from '../../../js/config'
+
+    export default {
+        props:{
+            operation:{
+                type:Object
+            }
+        },
+        data(){
+            return {
+                icon:""
+            }
+        },
+        methods:{
+            getImageUrl(url){
+                let _url = Config.serverConfig.engineService + "/stream?filePath=";
+                if (Util.isEmpty(url)) {
+                    return "";
+                }
+                let _array = url.split("||");
+                if (Array.isArray(_array)) {
+                    if (_array.length > 0) {
+                        return _url + _array[0];
+                    } else {
+                        return _url + url;
+                    }
+                } else {
+                    return _url + url;
+                }
+            }
+        },
+        watch:{
+            "operation"(val){
+                if(val.icon){
+                    this.icon = val.icon;
+                }
+            }
+        }
+    }
+</script>
+
