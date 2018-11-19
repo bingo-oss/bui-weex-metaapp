f786a3cae64509b889b936ccb1fecbcea9d30438
贺嘉文 <hejw@bingosoft.net>
2018-11-19 15:22:41 +0800

    新增文本部件

---
+++ src/widgets/meta-widget-text/meta-widget-text.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <text :style="textStyle">{{widgetParams.text}}</text>
+    </div>
+</template>
+<style>
+    .text{ padding-left: 10px; font-size: 24px;}
+</style>
+<script>
+    import buiweex from "bui-weex";
+    module.exports = {
+        props: {
+            widgetParams: {
+                type: Object,
+                required: true
+            }
+        },
+        computed: {
+            "textStyle"(){
+                return this.widgetParams.textStyle?JSON.parse(this.widgetParams.textStyle):{}
+            }
+        },
+        data () {
+            return {
+            }
+        },
+        methods: {
+        },
+        mounted(){}
+    }
+</script>
