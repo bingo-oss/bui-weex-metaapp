c49d180d49173ff8a2d1c3fcd02b63c27e53f189
贺嘉文 <hejw@bingosoft.net>
2019-04-30 17:39:33 +0800

    读取配置方式调整

--- src/js/config.js
+++ src/js/config.js
@@ -1,12 +1,11 @@
 const linkapi = require('linkapi');
+const storage = weex.requireModule('storage');
 import i18n from '../js/i18n/index';
 import buiweex from 'bui-weex';
 import ajax from '../js/ajax.js';
-const configServerUrl =
-    "http://10.200.84.125:9596/api/web.json";
-    /*"https://appfactoryservice.bingolink.biz/services/tool/system/config";*/
-    /*"https://developer.bingosoft.net:12100/services/tool/system/config";*/
-    /*"http://20.97.71.73/services/tool/system/config";*/
+import metaservice from "../js/metadata/metaservice"
+import metabase from "../js/metadata/metabase"
+const configServerUrl = "https://saaslinkdev.projects.bingosoft.net:22283/services";
 /**
  * 在开发调试时，可以将 debug 设为 true，并设置相应的 token，viewId 和 formId 来调试
  * 发布时记得将 debug 设为 false
@@ -14,8 +13,8 @@ const configServerUrl =
  */
 var configData = null;
 export default {
-    debug: true,
-    token: 'c2Fhc3Nzbzo2NTA0YTE5NS04NWEzLTRlZTctYWQ2ZS05NGVkYTQ0YmZjMTk',
+    debug: false,
+    token: 'c2Fhc3Nzbzo1Y2MyZDQzZS03YTZlLTRiOTgtOTUyMy00N2YxMDUzNDVjZjg',
     configFilename: 'config.json',
     serverConfig: {},
     // 读取与 list.weex.js、form.weex.js 同级的配置文件
@@ -26,78 +25,56 @@ export default {
                 this.serverConfig = configData;
                 resolve(configData);
             } else {
-/*                configData=Object.assign({},{
-                    "configServerUrl":configServerUrl,
-                    "blogApi": "",
-                    "uamUrl": "",
-                    "engineService": ""
-                });
-                this.serverConfig = {};
-                resolve(configData);*/
-
-                //读取全局配置地址方式
-            ajax.get(configServerUrl).then((resp) => {
-                resp.data.apiBaseUrl = "http://10.200.84.125:9596/api"
-                resp.data["service.menu.endpoint"] = "http://10.200.84.125:9596/api" 
-                resp.data["service.metad.api.endpoint"] = "http://10.200.84.125:9596/api"
-                configData=Object.assign(resp.data,{
-                    "configServerUrl":configServerUrl,
-                    "blogApi": resp.data["service.blog.endpoint"],
-                    "uamUrl": resp.data["service.link.endpoint"],
-                    "engineService": resp.data["service.gateway.endpoint"]+"/services"
-                });
-                this.serverConfig = configData;
-                resolve(configData);
-            },()=>{
-                this.serverConfig = {};
-                resolve({})
-            });
-                //end
-
-                 /*//读取文件config.json 配置方式
-                 contextPath = contextPath.replace('file://', ''); // 消除 file:
-                let absPath = `${contextPath}/${this.configFilename}`;
-                linkapi.readTextFromFile(absPath, 'utf-8', data => {
-                    try {
-                        if (data.configServerUrl) {
-                            //获取服务端的配置
-                            ajax.get(data.configServerUrl).then((resp) => {
-                                configData = Object.assign(data, resp.data, {
-                                    "blogApi": resp.data["service.blog.endpoint"],
-                                    "uamUrl": resp.data["service.link.endpoint"],
-                                    "engineService": resp.data["service.gateway.endpoint"] + "/services"
-                                });
-                                this.serverConfig = configData;
-                                resolve(configData);
-                            }, (err) => {
-                                reject(err);
-                            })
-                        } else {
-                            configData = data;
+                this.getProjectId().then((projectId)=>{
+                    //读取全局配置地址方式
+                    metaservice.getProject(projectId).then((projectInfo)=>{
+                        ajax.get(`${projectInfo.engine.externalUrl}/web.json`).then((resp) => {
+                            configData=Object.assign(resp.data,{
+                                "configServerUrl":configServerUrl,
+                                "blogApi": resp.data["service.blog.endpoint"],
+                                "uamUrl": resp.data["service.link.endpoint"],
+                                "engineService": resp.data["service.gateway.endpoint"]+"/services"
+                            });
+                            this.serverConfig = configData;
                             resolve(configData);
-                        }
-                    } catch (err) {
-                        reject(err);
-                    }
-                }, err => {
-                    reject(err);
+                        },()=>{
+                            this.serverConfig = {};
+                            resolve({})
+                        });
+                    },(e)=>{});
                 })
-                 //end*/
+                //end
             }
         })
 
     },
     getMetaServiceUrl() {
         return new Promise((resolve, reject) => {
-            this.readRuntimeConfig().then((c) => {
+            resolve(configServerUrl+"/metaservice")
+           /* contextPath = contextPath.replace('file://', ''); // 消除 file:
+            let absPath = `${contextPath}/${this.configFilename}`;
+            linkapi.readTextFromFile(absPath, 'utf-8', data => {
+                try {
+                    if (data.configServerUrl) {
+                        //获取服务端的配置
+                        resolve(data.configServerUrl+"/metaservice")
+                    }
+                } catch (err) {
+                    reject(err);
+                }
+            }, err => {
+                reject(err);
+            })*/
+            /*this.readRuntimeConfig().then((c) => {
                 var url = c['service.metabase.endpoint'];
+                debugger;
                 if (url) {
                     resolve(url);
                 } else {
                     buiweex.toast(i18n.noMetadataConfigUrl);
                     reject();
                 }
-            });
+            });*/
         });
     },
     getApiBaseUrl() {
@@ -123,6 +100,32 @@ export default {
                     buiweex.toast(i18n.noMetadataConfigUrl);
                     reject();
                 }
+            });
+        });
+    },
+    getProjectId(){
+        //获取项目id
+        return new Promise((resolve, reject) => {
+            let pageParam = buiweex.getPageParams();
+            storage.getItem("pageProjectId",({data})=> {
+                //先读取缓存上的,若没有怎读取页面上的,以页面参数优先
+                let projectId = data;
+                if (pageParam && pageParam.projectId) {
+                    //页面上带上了项目id
+                    projectId = pageParam.projectId
+                    storage.setItem("pageProjectId", pageParam.projectId)
+                }
+                resolve(projectId)
+            });
+        });
+    },
+    getMetabase(){
+        //获取实体对象
+        return new Promise((resolve, reject) => {
+            this.getProjectId().then(projectId=>{
+                metabase.initMetabase(projectId).then(ddd=>{
+                    resolve(metabase);
+                })
             });
         });
     }
--- src/js/page/page_service.js
+++ src/js/page/page_service.js
@@ -1,7 +1,6 @@
 import ax from '../ajax.js';
 import config from '../config.js';
 import buiweex from 'bui-weex';
-
 const linkapi = require('linkapi');
 var isIPhone = (weex.config.env.deviceModel.indexOf("iPhone") != -1)
 var isAndroid = (weex.config.env.deviceModel.indexOf("iPhone") == -1)
@@ -10,22 +9,17 @@ const pageService = {
         if (!byOperation) byOperation = false;
         return new Promise(function (resolve, reject) {
             config.readRuntimeConfig().then(runtimeConfig => {
-                /*var testPath=`https://saaslinkdev.projects.bingosoft.net:22283/web/test_page/${pageId}.json`;
-                ax.get(testPath).then((data) => {
-                    resolve(data.data);
-                });*/
                 let mobileType = "";
                 if (isIPhone) {
                     mobileType = 2
                 } else if (isAndroid) {
                     mobileType = 1
                 }
-
-                ax.get(runtimeConfig["service.metabase.endpoint"] + `/mp_page/${pageId}?terminalType=${mobileType}&byOperation=${byOperation}`)
+                ax.get(runtimeConfig["apiBaseUrl"] + `/$pages/${pageId}`)
                     .then(({data}) => {
                         resolve(data);
                     }, (error) => {
-                        ax.get(`http://10.200.210.4:8686/dist/test_page/${pageId}.json`)
+                        ax.get(`${buiweex.getContextPath()}/test_page/${pageId}.json`)
                             .then(({data}) => {
                                 resolve(data);
                             }, () => {
